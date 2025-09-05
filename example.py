c9316c299b0b078b71cadf20e6571c3c7c90db45
Anandakrishnamurthy93 <anandakumsi93@gmail.com>
2025-09-05 23:11:54 +0530

    Update example.py

--- example.py
+++ example.py
@@ -1,1 +1,17 @@
+import pandas as pd
+import numpy as np
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
 print("Hello Programmer")
+
+x1=np.arange(1,6)
+y1=x1*2
+y2=x1**2
+
+print(x1)
+print(y1)
+print(y2)
